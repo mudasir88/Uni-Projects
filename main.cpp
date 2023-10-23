8cc15e61c4e88461395f3cf5a3561759d2dfa402
Mudasir Ahmad <mudasirahmad1031@gmail.com>
2023-10-24 01:30:28 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+ int num1;
+ cin>> num1;
+ 
+ int num2;
+ cin>> num2;
+ 
+ cout<< num1 + num2 ;
+
+  return 0;
+}
